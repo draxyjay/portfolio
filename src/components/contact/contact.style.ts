fb0842014911acf4ae250d6df3b53e347545374b
Jay Patel <draxyjay@gmail.com>
2020-09-15 19:35:34 +0200

    Fix body size

--- src/components/contact/contact.style.ts
+++ src/components/contact/contact.style.ts
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
 export const ContactContainer = styled.div`
-	height: 100%;
 	display: flex;
 	flex-direction: column;
 	width: 200px;
---
+++ src/components/logospinner/logospinner.style.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Spinner = styled.div`
+	width: 100%;
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	justify-items: center;
+	align-items: center;
+`;
